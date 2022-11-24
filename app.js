b1d2aeae5af85e504da9c1f85b8718d0bc5feffb
LuizEddie <contato.luiz1997@gmail.com>
2022-11-24 19:39:35 -0300

    Aula 286

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-class Despesa{
+class Despesa {
 
-    constructor(ano, mes, dia, tipo, descricao, valor){
+    constructor(ano, mes, dia, tipo, descricao, valor) {
         this.ano = ano;
         this.mes = mes;
         this.dia = dia;
@@ -9,9 +9,9 @@ class Despesa{
         this.valor = valor;
     }
 
-    validarDados(){
-        for(let i in this){
-            if(this[i] == undefined || this[i] == '' || this[i] == null){
+    validarDados() {
+        for (let i in this) {
+            if (this[i] == undefined || this[i] == '' || this[i] == null) {
                 return false;
             }
         }
@@ -21,30 +21,48 @@ class Despesa{
 
 }
 
-class BD{
-    constructor(){
+class BD {
+    constructor() {
         let id = localStorage.getItem('id');
 
-        if(id === null){
+        if (id === null) {
             localStorage.setItem('id', 0);
         }
     }
 
-    getProximoID(){
+    getProximoID() {
         let proximoId = localStorage.getItem('id');
-        return parseInt(proximoId)+1; //null
+        return parseInt(proximoId) + 1; //null
     }
 
-    gravar(d){
+    gravar(d) {
         let id = this.getProximoID();
         localStorage.setItem(id, JSON.stringify(d));
         localStorage.setItem('id', id);
+    }
+
+    recuperarTodosRegistros() {
+
+        let despesas = Array();
+        let id = localStorage.getItem('id');
+
+        for (let i = 1; i <= id; i++) {
+            let despesa = JSON.parse(localStorage.getItem(i));
+
+            if (despesa === null) {
+                continue
+            }
+
+            despesas.push(despesa);
+        }
+
+        return despesas;
     }
 }
 
 let bd = new BD;
 
-function cadastrarDespesa(){
+function cadastrarDespesa() {
 
     let ano = document.getElementById('ano');
     let mes = document.getElementById('mes');
@@ -54,8 +72,8 @@ function cadastrarDespesa(){
     let valor = document.getElementById('valor');
 
     let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value);
-    
-    if(despesa.validarDados()){
+
+    if (despesa.validarDados()) {
         bd.gravar(despesa);
         $("#modalRegistraDespesa").modal('show');
         document.getElementById('modalHeader').className = 'modal-header text-success';
@@ -64,7 +82,7 @@ function cadastrarDespesa(){
         document.getElementById('buttonVoltar').className = 'btn btn-success';
         document.getElementById('buttonVoltar').innerHTML = 'Voltar';
 
-    }else{
+    } else {
         $("#modalRegistraDespesa").modal('show');
         document.getElementById('modalHeader').className = 'modal-header text-danger';
         document.getElementById('exampleModalLabel').innerHTML = 'Erro na Gravação';
@@ -73,4 +91,41 @@ function cadastrarDespesa(){
         document.getElementById('buttonVoltar').innerHTML = 'Voltar e corrigir';
 
     }
+}
+
+function carregaListaDespesas() {
+    let despesas = Array();
+    despesas = bd.recuperarTodosRegistros();
+
+    let listaDespesas = document.getElementById("listaDespesas");
+
+    despesas.forEach((d) => {
+        console.log(d);
+
+        let linha = listaDespesas.insertRow();
+        linha.insertCell(0).innerHTML = `${d['dia']}/${d['mes']}/${d['ano']}`;
+
+        switch (d['tipo']) {
+            case '1':
+                d['tipo'] = 'Alimentação';
+                break;
+            case '2':
+                d['tipo'] = 'Educação';
+                break;
+            case '3':
+                d['tipo'] = 'Lazer';
+                break;
+            case '4':
+                d['tipo'] = 'Saúde';
+                break;
+            case '5':
+                d['tipo'] = 'Transporte';
+                break;
+        }
+
+        linha.insertCell(1).innerHTML = d['tipo'];
+        linha.insertCell(2).innerHTML = d['descricao'];
+        linha.insertCell(3).innerHTML = d['valor'];
+
+    });
 }
